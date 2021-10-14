7991af090610c1b7ed01fde91eda4def96e25b43
Philippe Bernard <philippe.bernard@gmail.com>
2021-10-14 16:57:01 +0200

    List templates at build

---
+++ src/build-templates-list.ts
@@ -0,0 +1,11 @@
+import classicFs from 'fs';
+import path from 'path/posix';
+const fs = classicFs.promises;
+
+fs.readdir('resoc-templates', { withFileTypes: true })
+.then(paths => paths.filter(path => path.isDirectory()))
+.then(dirs => fs.writeFile(
+  'resoc-templates/templates-list.json',
+  JSON.stringify(dirs.map(dir => dir.name))
+))
+.then(() => console.log("Done!"));
