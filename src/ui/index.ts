c566b8c9d020a88dd1278d9910df0d9bc66d96de
Philippe Bernard <philippe.bernard@gmail.com>
2021-10-18 14:23:46 +0200

    Minimalist React app

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello");
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  React.createElement(App, {}, null),
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: './src/ui/index.ts',
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'out'),
