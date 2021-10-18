5264d42f69870eeca3aae5f80a801f4878380e22
Philippe Bernard <philippe.bernard@gmail.com>
2021-10-18 15:01:00 +0200

    Load templates and show them

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import ListGroup from 'react-bootstrap/ListGroup'
 
-const App = () => (
-  <p>TODO!!!</p>
-);
+type Template = {
+  name: string;
+};
+
+const App = () => {
+  const [ templates, setTemplates ] = useState<Template[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get('/templates');
+      const templates: any = response.data;
+      setTemplates(templates);
+    })();
+  });
+
+  if (!templates) {
+    return (
+      <p>
+        Loading...
+      </p>
+    );
+  }
+
+  return (
+    <ListGroup>
+      {templates.map(t => (
+        <ListGroup.Item action>
+          {t.name}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
 
 export default App;
